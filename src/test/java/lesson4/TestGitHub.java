fe499475307e35c5f73dfa0c719d384c40a4de86
d.chizhova <dchizhova68@gmail.com>
2023-11-07 23:23:00 +0400

    disable holdBrowserOpen

--- src/test/java/lesson4/TestGitHub.java
+++ src/test/java/lesson4/TestGitHub.java
@@ -15,7 +15,7 @@ public class TestGitHub {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
     }
     @Test
     void searchSoftAssertion() {
