3d9ac95eea48bdfc3881e02e8d3d08c09c9c7a29
d.chizhova <dchizhova68@gmail.com>
2024-01-04 20:04:56 +0400

    fix test_file

--- src/test/java/dchizhova68/lesson11/ParsingJsonFile.java
+++ src/test/java/dchizhova68/lesson11/ParsingJsonFile.java
@@ -2,10 +2,11 @@ package dchizhova68.lesson11;
 
 import com.google.gson.Gson;
 import dchizhova68.lesson11.model.Transactions;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.InputStreamReader;
+import org.assertj.core.api.Assertions;
 
 public class ParsingJsonFile {
     private ClassLoader classLoader = ParsingJsonFile.class.getClassLoader();
@@ -17,12 +18,10 @@ public class ParsingJsonFile {
                 classLoader.getResourceAsStream("transaction.json")
         )) {
             Transactions actual = objectMapper.readValue(reader, Transactions.class);
-            Assertions.assertEquals("4457", actual.getUserId());
+            Assertions.assertThat(actual.getUserId()).isEqualTo("4457");
 
-            Assertions.assertEquals(20,
-                    actual.getTransactions()[0].getCount());
-            Assertions.assertEquals("Автоматическая",
-                    actual.getTransactions()[1].getTransactionType());
+            Assertions.assertThat(actual.getTransactions()[0].getCount()).isEqualTo(20);
+            Assertions.assertThat(actual.getTransactions()[1].getTransactionType()).isEqualTo("Автоматическая");;
         }
     }
 }
